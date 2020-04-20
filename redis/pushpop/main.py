4ab832fbd16e6587dacc99afb1079be35a9ff2e8
ringsaturn <ringsaturn.me@gmail.com>
2020-04-20 13:29:20 +0800

    docs(redis): push & pop demo

---
+++ redis/pushpop/main.py
@@ -0,0 +1,20 @@
+import redis
+
+client = redis.Redis()
+
+# Append to list
+res = client.rpush("test_list_01", "hello")
+print(res)
+res = client.rpush("test_list_01", "world")
+print(res)
+
+# Show list
+res = client.lrange("test_list_01", 0, -1)
+print(res)
+
+# Get from start
+res = client.lpop("test_list_01")
+print(res)
+
+res = client.lpop("test_list_01")
+print(res)
